83adc6bfefb1a818b11cb6d81328fa182442fda3
s22002 <s22002@std.it-college.ac.jp>
2023-02-09 06:17:58 +0000

    8

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import styles from 'styles/nav.module.css'
 
 const Nav = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev)
+  }
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      <button className={styles.btn} onClick={toggleNav}>MENU</button>
       <ul className={styles.list}>
         <li>
           <Link legacyBehavior href='/'>
